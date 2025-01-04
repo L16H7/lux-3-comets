15cadc4166fc6a71f62a9aace21a3f48ecf6aa9c
L16H7 <lightcode2018@gmail.com>
2025-01-04 00:14:52 -0600

    ok

--- trainer/train.py
+++ trainer/train.py
@@ -321,17 +321,17 @@ def make_train(config: Config):
                         sap_ranges=meta_env_params.unit_sap_range,
                     )
 
-                    p1_values, p1_critic_hstates = critic_train_state.apply_fn(
-                        critic_train_state.params,
-                        p1_prev_critic_hstates,
-                        {
-                            "states": jnp.expand_dims(p1_states, axis=0),
-                            "match_steps": jnp.expand_dims(p1_episode_info[:, 0], axis=[0, -1]),
-                            "matches": jnp.expand_dims(p1_episode_info[:, 1], axis=[0, -1]),
-                            "team_points": jnp.expand_dims(p1_episode_info[:, 2], axis=[0, -1]),
-                            "opponent_points": jnp.expand_dims(p1_episode_info[:, 3], axis=[0, -1]),
-                        }
-                    )
+                    # p1_values, p1_critic_hstates = critic_train_state.apply_fn(
+                    #     critic_train_state.params,
+                    #     p1_prev_critic_hstates,
+                    #     {
+                    #         "states": jnp.expand_dims(p1_states, axis=0),
+                    #         "match_steps": jnp.expand_dims(p1_episode_info[:, 0], axis=[0, -1]),
+                    #         "matches": jnp.expand_dims(p1_episode_info[:, 1], axis=[0, -1]),
+                    #         "team_points": jnp.expand_dims(p1_episode_info[:, 2], axis=[0, -1]),
+                    #         "opponent_points": jnp.expand_dims(p1_episode_info[:, 3], axis=[0, -1]),
+                    #     }
+                    # )
 
                     p0_relic_mask = observations['player_0'].relic_nodes != -1
                     p0_new_discovered_relic_nodes = jnp.where(
@@ -376,23 +376,23 @@ def make_train(config: Config):
                     p1_rewards = rewards[:, 1, :].reshape(1, -1, 1)
 
                     transition = Transition(
-                        agent_states=jnp.squeeze(jnp.concat([p0_agent_states, p1_agent_states], axis=1), axis=0),
-                        observations=jnp.squeeze(jnp.concat([p0_agent_observations, p1_agent_observations], axis=1), axis=0),
-                        states=jnp.concat([p0_states, p1_states], axis=0),
-                        episode_info=jnp.concat([p0_episode_info, p1_episode_info], axis=0),
-                        agent_episode_info=jnp.concat([p0_agent_episode_info, p1_agent_episode_info], axis=0),
-                        actions=jnp.concat([p0_actions, p1_actions], axis=0),
-                        prev_actions=jnp.squeeze(jnp.concat([p0_prev_actions, p1_prev_actions], axis=1), axis=0),
-                        prev_points=jnp.squeeze(jnp.concat([p0_prev_points, p1_prev_points], axis=1), axis=[0, 2]),
-                        log_probs=jnp.squeeze(jnp.concat([p0_log_probs, p1_log_probs], axis=1), axis=0),
-                        values=jnp.squeeze(jnp.concat([p0_values, p1_values], axis=1), axis=[0, 2]),
-                        agent_positions=jnp.squeeze(jnp.concat([p0_agent_positions, p1_agent_positions], axis=1), axis=0),
-                        rewards=jnp.squeeze(jnp.concat([p0_rewards, p1_rewards], axis=1), axis=[0, 2]),
-                        dones=jnp.logical_or(terminated["player_0"], truncated["player_0"]).repeat(2 * config.n_agents),
-                        units_mask=jnp.concat([p0_units_mask.reshape(-1), p1_units_mask.reshape(-1)], axis=0),
-                        logits1_mask=jnp.squeeze(jnp.concat([p0_logits_mask[0], p1_logits_mask[0]], axis=1), axis=0),
-                        logits2_mask=jnp.squeeze(jnp.concat([p0_logits_mask[1], p1_logits_mask[1]], axis=1), axis=0),
-                        logits3_mask=jnp.squeeze(jnp.concat([p0_logits_mask[2], p1_logits_mask[2]], axis=1), axis=0),
+                        agent_states=jnp.squeeze(p0_agent_states, axis=0),
+                        observations=jnp.squeeze(p0_agent_observations, axis=0),
+                        states=p0_states,
+                        episode_info=p0_episode_info,
+                        agent_episode_info=p0_agent_episode_info,
+                        actions=p0_actions,
+                        prev_actions=jnp.squeeze(p0_prev_actions, axis=0),
+                        prev_points=jnp.squeeze(p0_prev_points, axis=[0, 2]),
+                        log_probs=jnp.squeeze(p0_log_probs, axis=0),
+                        values=jnp.squeeze(p0_values, axis=[0, 2]),
+                        agent_positions=jnp.squeeze(p0_agent_positions, axis=0),
+                        rewards=jnp.squeeze(p0_rewards, axis=[0, 2]),
+                        dones=jnp.logical_or(terminated["player_0"], truncated["player_0"]).repeat(config.n_agents),
+                        units_mask=p0_units_mask.reshape(-1),
+                        logits1_mask=jnp.squeeze(p0_logits_mask[0], axis=0),
+                        logits2_mask=jnp.squeeze(p0_logits_mask[1], axis=0),
+                        logits3_mask=jnp.squeeze(p0_logits_mask[2], axis=0),
                         env_information=env_information,
                     )
 
@@ -488,21 +488,21 @@ def make_train(config: Config):
                     }
                 )
 
-                p1_last_values, _ = critic_train_state.apply_fn(
-                    critic_train_state.params,
-                    p1_prev_critic_hstates,
-                    {
-                        "states": jnp.expand_dims(p1_states, axis=0),
-                        "match_steps": jnp.expand_dims(p1_episode_info[:, 0], axis=[0, -1]),
-                        "matches": jnp.expand_dims(p1_episode_info[:, 1], axis=[0, -1]),
-                        "team_points": jnp.expand_dims(p1_episode_info[:, 2].astype(jnp.int32), axis=[0, -1]),
-                        "opponent_points": jnp.expand_dims(p1_episode_info[:, 3].astype(jnp.int32), axis=[0, -1]),
-                    }
-                )
+                # p1_last_values, _ = critic_train_state.apply_fn(
+                #     critic_train_state.params,
+                #     p1_prev_critic_hstates,
+                #     {
+                #         "states": jnp.expand_dims(p1_states, axis=0),
+                #         "match_steps": jnp.expand_dims(p1_episode_info[:, 0], axis=[0, -1]),
+                #         "matches": jnp.expand_dims(p1_episode_info[:, 1], axis=[0, -1]),
+                #         "team_points": jnp.expand_dims(p1_episode_info[:, 2].astype(jnp.int32), axis=[0, -1]),
+                #         "opponent_points": jnp.expand_dims(p1_episode_info[:, 3].astype(jnp.int32), axis=[0, -1]),
+                #     }
+                # )
 
                 advantages, targets = calculate_gae(
                     transitions,
-                    jnp.concat([p0_last_values, p1_last_values], axis=0).repeat(config.n_agents, axis=1).reshape(-1),
+                    p0_last_values.repeat(config.n_agents, axis=1).reshape(-1),
                     config.gamma,
                     config.gae_lambda
                 )
